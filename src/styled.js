f5837b9fba6c053561afce48c28b7ccd12816eb6
gabrielmina118 <gabrielmina118@gmail.com>
2024-02-18 19:42:31 -0300

    feat: :art: Alteração do containerPAge

--- src/styled.js
+++ src/styled.js
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 export const Main = styled.div`
     display: flex;
-    justify-content: space-between;
     width: 100vw;
-    min-height: 100vh;
+    height: 100vh;
+`;
+export const ContainerPage = styled.div`
+    display: flex;
+    margin-left: var(--menu-size-max);
+    overflow-y: auto;
+    flex-grow: 1;
+    height: 100%;
+    padding-top: 48px;
+    @media (max-width: 768px) {
+        margin-left: 0;
+    }
 `;
