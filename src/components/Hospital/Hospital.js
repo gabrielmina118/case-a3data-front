11baa910b04cd7f867c55aa3efebbb928ca19b9c
gabrielmina118 <gabrielmina118@gmail.com>
2024-02-18 21:25:14 -0300

    feat: :sparkles: Implementação do componente de hospital

--- src/components/Hospital/Hospital.js
+++ src/components/Hospital/Hospital.js
@@ -1,8 +1,24 @@
+import React from "react";
+import TableAction from "../TableAction/TableAction";
 const Hospital = () => {
+    const mapColumns = [
+        { text: "Id", key: "id", edit: false },
+        {
+            text: "Nome do hospital",
+            key: "name",
+            edit: true,
+            type: "text",
+        },
+    ];
     return (
-        <div>
-            <p>Hospital</p>
-        </div>
+        <>
+            <TableAction
+                title={"Hospitais Cadastradas"}
+                mapColumns={mapColumns}
+                entity={"hospital"}
+                path={"hospital"}
+            />
+        </>
     );
 };
 
